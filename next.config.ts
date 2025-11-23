84330d6b2b4d707243ea5bd1cf8aed0dd161b597
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-23 15:33:33 +0700

    MENAMBAHKAN ENDPOINT API UNTUK MENDAPATKAN EVENT BERDASARKAN SLUG | UPDATE FETCH EVENTS DI HALAMAN UTAMA
    *). Membuat endpoint baru `GET /api/[slug]` untuk mengambil data event berdasarkan slug.
    *). Mengupdate fetching data event di `app/page.tsx` untuk mengambil daftar event dari API.
    *). Memperbarui konfigurasi Next.js (`next.config.ts`) agar mendukung pemuatan gambar dari domain Unsplash.

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,11 @@ const nextConfig: NextConfig = {
             {
                 protocol: 'https',
                 hostname: 'res.cloudinary.com',
-            }
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+            },
         ]
     },
 
